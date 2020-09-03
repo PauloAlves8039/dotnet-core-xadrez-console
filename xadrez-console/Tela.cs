06ca234f177daf7668f12c0273698441a7ba770f
PauloAlves8039 <pj38alves@gmail.com>
2020-09-03 14:24:12 -0300

    Implementando impressão do tabuleiro

---
+++ xadrez-console/Tela.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    /// <summary>
+    /// Classe responsável por imprimir o tabuleiro na tela.
+    /// </summary>
+    class Tela
+    {
+        /// <summary>
+        /// Método responsável pela impressão o tabuleiro em tela.
+        /// </summary>
+        /// <param name="tab">Recebe uma instância da classe Tabuleiro.</param>
+        public static void ImprimirTabuleiro(Tabuleiro tab) 
+        {
+            for (int i = 0; i < tab.Linhas; i++) 
+            {
+                for (int j = 0; j < tab.Colunas; j++) 
+                {
+                    if (tab.RetornarPeca(i, j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else 
+                    {
+                        Console.Write(tab.RetornarPeca(i, j) + " ");
+                    }                    
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -15,6 +15,17 @@
             Colunas = colunas;
             pecas = new Peca[linhas, colunas];
         }
+
+        /// <summary>
+        /// Método responsável por retornar uma matriz de peças.
+        /// </summary>
+        /// <param name="linha">Define o parâmetro linha.</param>
+        /// <param name="coluna">Define o parâmetro coluna.</param>
+        /// <returns>Uma matriz de peças.</returns>
+        public Peca RetornarPeca(int linha, int coluna) 
+        {
+            return pecas[linha, coluna];
+        }
     }
 
 }
