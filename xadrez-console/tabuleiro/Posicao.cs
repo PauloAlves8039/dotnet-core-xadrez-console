df866ef06967d38ba576af2fdf595256bcf3dbea
PauloAlves8039 <pj38alves@gmail.com>
2020-09-03 12:33:24 -0300

    Criando a classe Posicao

---
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,33 @@
+﻿namespace tabuleiro
+{
+    /// <summary>
+    /// Classe responsável por definir a posição das peças no tabuleiro.
+    /// </summary>
+    public class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        /// <summary>
+        /// Método responsável por definir valores das posições.
+        /// </summary>
+        /// <param name="linha">Define o parâmetro linha.</param>
+        /// <param name="coluna">Define o parâmetro coluna.</param>
+        public void DefinirValores(int linha, int coluna) 
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return Linha + "," + Coluna;
+        }
+    }
+}
