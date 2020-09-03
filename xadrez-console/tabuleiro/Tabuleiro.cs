1d539da6643b0102cd272608384c576963c9dba2
PauloAlves8039 <pj38alves@gmail.com>
2020-09-03 13:37:38 -0300

    Criando a classe Tabuleiro

---
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xadrez_console.tabuleiro
+{
+    /// <summary>
+    /// Classe responsável pela atribuição do tabuleiro.
+    /// </summary>
+    class Tabuleiro
+    {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] Pecas;
+    }
+
+    public Tabuleiro(int linhas, int colunas) 
+    {
+        Linhas = linhas;
+        Colunas = colunas;
+        Pecas = new Peca[linhas, colunas];
+    }
+}
