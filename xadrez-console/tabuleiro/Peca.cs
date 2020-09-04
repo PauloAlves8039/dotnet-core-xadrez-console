99685c6c4de4da8800cc79d5d89ca4f13ed89d4a
PauloAlves8039 <pj38alves@gmail.com>
2020-09-04 19:49:46 -0300

    Implementação inicial da classe Rei

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -8,13 +8,13 @@
         public Posicao Posicao { get; set; }
         public Cor Cor { get; protected set; }
         public int QteMovimentos { get; protected set; }
-        public Tabuleiro Tabuleiro { get; set; }
+        public Tabuleiro Tab { get; set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            Posicao = posicao;
+            Posicao = null;
             Cor = cor;
-            Tabuleiro = tabuleiro;
+            Tab = tab;
             QteMovimentos = 0;
         }
     }
---
+++ xadrez-console/xadrez/Rei.cs
@@ -0,0 +1,20 @@
+﻿using tabuleiro;
+
+namespace xadrez_console.xadrez
+{
+    /// <summary>
+    /// Classe responsável por representar a entidade Rei.
+    /// </summary>
+    class Rei : Peca
+    {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
