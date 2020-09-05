4cebccdb923d219c831055a6830ca0b36b4c26b2
PauloAlves8039 <pj38alves@gmail.com>
2020-09-05 14:48:13 -0300

    Criando uma exceção personalizada para o tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -28,6 +28,27 @@
         }
 
         /// <summary>
+        /// Método responsável por retornar uma matriz de peças.
+        /// </summary>
+        /// <param name="pos">Define uma instância da classe Posicao.</param>
+        /// <returns>Uma matriz de peças.</returns>
+        public Peca RetornarPeca(Posicao pos) 
+        {
+            return pecas[pos.Linha, pos.Coluna];
+        }
+
+        /// <summary>
+        /// Método para verificar se peça existe em determinada posição.
+        /// </summary>
+        /// <param name="pos">Define uma instância da classe Posicao.</param>
+        /// <returns>A posição válida da peça.</returns>
+        public bool ExistePeca(Posicao pos) 
+        {
+            ValidarPosicao(pos);
+            return RetornarPeca(pos) != null;
+        }
+
+        /// <summary>
         /// Método para colocar peça no tabuleiro.
         /// </summary>
         /// <param name="p">Define uma instância da classe Peca.</param>
@@ -36,6 +57,32 @@
         {
             pecas[pos.Linha, pos.Coluna] = p;
             p.Posicao = pos;
+        }
+
+        /// <summary>
+        /// Método para testar se uma posição é válida.
+        /// </summary>
+        /// <param name="pos">Define uma instância da classe Posicao.</param>
+        /// <returns>Uma posição válida no tabuleiro.</returns>
+        public bool PosicaoValida(Posicao pos) 
+        {
+            if (pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna >= Colunas) 
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Método para válidar uma posição. 
+        /// </summary>
+        /// <param name="pos">Define uma instância da classe Posicao.</param>
+        public void ValidarPosicao(Posicao pos) 
+        {
+            if (!PosicaoValida(pos)) 
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 
---
+++ xadrez-console/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace tabuleiro
+{
+    /// <summary>
+    /// Classe responsável por tratamento de exceção personalizada referente a entidade Tabuleiro.
+    /// </summary>
+    class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
